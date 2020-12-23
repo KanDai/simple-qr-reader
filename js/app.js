7e8697f663cd57c2edac71f7c1819e6788570db4
Daichi Kanke <kanke@tam-tam.co.jp>
2020-12-23 21:55:44 +0900

    clean: コード整理

--- js/app.js
+++ js/app.js
@@ -7,24 +7,27 @@ if (window.BarcodeDetector == undefined) {
     document.querySelector('#js-unsupported').classList.add('is-show')
 }
 
-const video  = document.querySelector('#js-video')
+const video = document.querySelector('#js-video')
 
 const checkImage = () => {
     const barcodeDetector = new BarcodeDetector()
-    barcodeDetector.detect(video)
-        .then(barcodes => {
-            if ( barcodes.length > 0 ) {
+    barcodeDetector
+        .detect(video)
+        .then((barcodes) => {
+            if (barcodes.length > 0) {
                 // QRコードの読み取りに成功したらモーダル開く
-                for (let barcode of barcodes){
-                    console.log(barcode)
+                for (let barcode of barcodes) {
                     openModal(barcode.rawValue)
                 }
             } else {
                 // QRコードが見つからなかったら再度実行
-                setTimeout(() => { checkImage() }, 200)
+                setTimeout(() => {
+                    checkImage()
+                }, 200)
             }
-        }).catch((e) => {
-            console.error("Barcode Detection failed, boo.")
+        })
+        .catch((e) => {
+            console.error('Barcode Detection failed, boo.')
         })
 }
 
@@ -33,29 +36,28 @@ navigator.mediaDevices
         audio: false,
         video: {
             facingMode: {
-                exact: 'environment'
-            }
-        }
+                exact: 'environment',
+            },
+        },
     })
-    .then(function(stream) {
+    .then((stream) => {
         video.srcObject = stream
-        video.onloadedmetadata = function(e) {
+        video.onloadedmetadata = () => {
             video.play()
             checkImage()
         }
     })
-    .catch(function(err) {
+    .catch((err) => {
         alert('Error!!')
     })
 
-const openModal = function(url) {
+const openModal = (url) => {
     document.querySelector('#js-result').innerText = url
     document.querySelector('#js-link').setAttribute('href', url)
     document.querySelector('#js-modal').classList.add('is-show')
 }
 
-document.querySelector('#js-modal-close')
-    .addEventListener('click', () => {
-        document.querySelector('#js-modal').classList.remove('is-show')
-        checkImage()
-    })
+document.querySelector('#js-modal-close').addEventListener('click', () => {
+    document.querySelector('#js-modal').classList.remove('is-show')
+    checkImage()
+})
