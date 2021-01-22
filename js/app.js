c9ec5147488589e7b2e9623b239520e6a1da1721
Daichi Kanke <kanke@tam-tam.co.jp>
2021-01-22 23:42:28 +0900

    JSの書き方を全体的に調整

--- js/app.js
+++ js/app.js
@@ -1,65 +1,89 @@
-const video = document.querySelector('#js-video')
+window.SQR = window.SQR || {}
 
-const checkImage = () => {
-    const barcodeDetector = new BarcodeDetector()
-    barcodeDetector
-        .detect(video)
-        .then((barcodes) => {
-            if (barcodes.length > 0) {
-                // QRコードの読み取りに成功したらモーダル開く
-                for (let barcode of barcodes) {
-                    openModal(barcode.rawValue)
-                }
-            } else {
-                // QRコードが見つからなかったら再度実行
-                setTimeout(() => {
-                    checkImage()
-                }, 200)
-            }
-        })
-        .catch((e) => {
-            console.error('Barcode Detection failed, boo.')
-        })
-}
-
-const initCamera = () => {
-    navigator.mediaDevices
-        .getUserMedia({
-            audio: false,
-            video: {
-                facingMode: {
-                    exact: 'environment',
-                },
-            },
-        })
-        .then((stream) => {
-            video.srcObject = stream
-            video.onloadedmetadata = () => {
-                video.play()
-                checkImage()
-            }
-        })
-        .catch((err) => {
-            alert('Error!!')
-        })
-}
-
-const openModal = (url) => {
-    document.querySelector('#js-result').innerText = url
-    document.querySelector('#js-link').setAttribute('href', url)
-    document.querySelector('#js-modal').classList.add('is-show')
-}
-
-document.querySelector('#js-modal-close').addEventListener('click', () => {
-    document.querySelector('#js-modal').classList.remove('is-show')
-    checkImage()
-})
-
-const initApp = () => {
-    if (!navigator.mediaDevices || !window.BarcodeDetector) {
+SQR.reader = (() => {
+    if (!navigator.mediaDevices) {
         document.querySelector('#js-unsupported').classList.add('is-show')
         return
     }
-    initCamera()
-}
-initApp()
+
+    const video = document.querySelector('#js-video')
+
+    const checkQR = () => {
+        if (window.BarcodeDetector) {
+            // @TODO BarcodeDetectorが使えるかどうかで処理を分岐させる
+        }
+
+        const barcodeDetector = new BarcodeDetector()
+        barcodeDetector
+            .detect(video)
+            .then((barcodes) => {
+                if (barcodes.length > 0) {
+                    // QRコードの読み取りに成功したらモーダル開く
+                    for (let barcode of barcodes) {
+                        SQR.modal.open(barcode.rawValue)
+                    }
+                } else {
+                    // QRコードが見つからなかったら再度実行
+                    setTimeout(checkQR, 200)
+                }
+            })
+            .catch((e) => {
+                console.error('Barcode Detection failed, boo.')
+            })
+    }
+
+    const initCamera = () => {
+        navigator.mediaDevices
+            .getUserMedia({
+                audio: false,
+                video: {
+                    facingMode: {
+                        exact: 'environment',
+                    },
+                },
+            })
+            .then((stream) => {
+                video.srcObject = stream
+                video.onloadedmetadata = () => {
+                    video.play()
+                    checkQR()
+                }
+            })
+            .catch((err) => {
+                alert('Error!!')
+            })
+    }
+
+    return {
+        initCamera,
+        checkQR,
+    }
+})()
+
+SQR.modal = (() => {
+    const result = document.querySelector('#js-result')
+    const link = document.querySelector('#js-result')
+    const modal = document.querySelector('#js-result')
+    const modalClose = document.querySelector('#js-modal-close')
+
+    const open = (url) => {
+        result.innerText = url
+        link.setAttribute('href', url)
+        modal.classList.add('is-show')
+    }
+
+    const close = () => {
+        modal.classList.remove('is-show')
+        SQR.reader.checkQR()
+    }
+
+    modalClose.addEventListener('click', () => {
+        close()
+    })
+
+    return {
+        open,
+    }
+})()
+
+if (SQR.reader) SQR.reader.initCamera()
